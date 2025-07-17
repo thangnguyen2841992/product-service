62d82e06419c863e409e9dd1221a255f1d3355c0
thangnguyen2841992 <ntthang4@cmcglobal.vn>
2025-07-17 22:25:08 +0700

    ll

--- src/main/java/com/order/product/service/cart/CartServiceImpl.java
+++ src/main/java/com/order/product/service/cart/CartServiceImpl.java
@@ -60,7 +60,7 @@ public class CartServiceImpl implements ICartService {
                 productCartNew.setQuantity(productCartForm.getQuantity());
                 productCartNew.setDateCreated(new Date());
                 productCartNew.setCartId(cartOptional.get().getCartId());
-                ProductCart newProductCartSave = this.productCartRepository.save(productCartNew);
+                this.productCartRepository.save(productCartNew);
             }
 
             return getCartResponseByUserId(cartForm.getUserId());
@@ -136,13 +136,5 @@ public class CartServiceImpl implements ICartService {
         return getCartResponseByUserId(cart.getUserId());
     }
 
-    private void getAllProductCartOfCart(Optional<Cart> cartOptional, CartResponse cartResponse) {
-        List<ProductCart> productCartList;
-        productCartList = this.productCartRepository.findAllProductCartByCartId(cartOptional.get().getCartId());
-        cartResponse.setCartId(cartOptional.get().getCartId());
-        cartResponse.setUserId(cartOptional.get().getUserId());
-        cartResponse.setDateCreated(cartOptional.get().getDateCreated());
-        cartResponse.setProductCartList(productCartList);
-    }
 
 }
